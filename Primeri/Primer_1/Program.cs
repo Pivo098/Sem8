10e74f303bf93f8d29e31b80eb2d1e2826804615
Ivan Pivnev <pivnev-1998@mail.ru>
2023-03-07 23:09:03 +0300

    sem8

---
+++ Primeri/Primer_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+int [,] GenerateArray(int r, int c)
+{
+    int[,] array = new int[r, c]; 
+    Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-9, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int [,] MoveFirstLast(int[,] array)
+{
+    int LastRow = array.GetLength(0) - 1;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        (array[LastRow, i],array[0,i]) = (array[0, i], array[LastRow, i]); // зеркальное изменение
+    }
+    return array;
+}
+
+int [,] MyArray = GenerateArray(5, 6);
+PrintArray(MyArray);
+System.Console.WriteLine();
+PrintArray(MoveFirstLast(MyArray));
---
+++ Primeri/Primer_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь 
+// содержит информацию о том, сколько раз встречается элемент входных данных. Значения элементов массива 0..9
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col]; // Создаем 2-мерный массив
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int[] FrequensyArray(int[,] array)
+{
+    int[] FreqArray =new int[10];
+    foreach (var item in array)
+    {
+        FreqArray [item]++; 
+    }
+    return FreqArray;
+}
+
+void PrintFreqArray(int[]frequanceDictionary)
+{
+    for (int i = 0; i < frequanceDictionary.Length; i++)
+    {
+        System.Console.WriteLine($"{i} встречается {frequanceDictionary[i]}  раз");
+    }
+}
+
+int[,]matrix = GenerateArray(4,4);
+PrintArray(matrix);
+PrintFreqArray(FrequensyArray (matrix));
